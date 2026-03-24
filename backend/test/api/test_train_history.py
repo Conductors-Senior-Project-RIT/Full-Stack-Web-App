439b26d45a3130674a69b06a928690be54f58102
Kaitlin Yandik <kmy1691@g.rit.edu>
2026-03-24 15:25:58 -0400

    test post

--- backend/test/api/test_train_history.py
+++ backend/test/api/test_train_history.py
@@ -38,12 +38,11 @@ class TestTrainHistory(unittest.TestCase):
         self.assertEqual(cm.exception.description, f"Provided {name} must be greater than {min_value} but was given {value}...")
     
     @patch("backend.src.api.train_history.db")
-    @patch("backend.src.api.train_history.RecordService")
-    def test_get_hot_success(self, mock_db, mock_rs):
+    def test_get_hot_success(self, mock_db):
         mock_db.session = Mock()
         hot_rec = [{
             "id": 1,
-            "date_rec": datetime.strptime('2001-02-04 01:23:45', "%Y-%m-%d %H:%M:%S"),
+            "date_rec": str(datetime.strptime('2001-02-04 01:23:45', "%Y-%m-%d %H:%M:%S")),
             "station_name": "test station",
             "symbol_id": None,
             "unit_addr": "5678",
@@ -52,8 +51,35 @@ class TestTrainHistory(unittest.TestCase):
             "parity": 'UNKNOWN',
             "verified": False
         }]
-        mock_rs.get_train_history = Mock(return_value=hot_rec)
-        response = self.client.get("/api/history", query_string={"type": 2, "id": 1})
-        self.assertEqual(response.status_code, 200)
+        with patch("backend.src.api.train_history.RecordService") as mock_rs:
+            instance = mock_rs.return_value
+            instance.get_train_history.return_value = hot_rec
+            response = self.client.get("/api/history", query_string={"type": 2, "id": 1})
+            self.assertEqual(response.status_code, 200)
+            instance.get_train_history.assert_called_with(1, 1)
+            self.assertEqual(response.json, hot_rec)
+    
+    @patch("backend.src.api.train_history.db")
+    def test_post_eot_success(self, mock_db):
+        mock_db.session = Mock()
+        eot_data = {
+            "type": 1,
+            "station_id": 1,
+            "unit_addr": "1234",
+            "brake_pressure": "UNKNOWN",
+            "motion": "UNKNOWN",
+            "marker_light": "UNKNOWN",
+            "turbine": "UNKNOWN",
+            "battery_cond": "UNKNOWN",
+            "battery_charge": "UNKNOWN",
+            "arm_status": "UNKNOWN",
+            "signal_strength": 1,
+        }
+        with patch("backend.src.api.train_history.RecordService") as mock_rs:
+            instance = mock_rs.return_value
+            instance.post_train_history.return_value = None
+            response = self.client.post("/api/history", json=eot_data)
+            self.assertEqual(response.status_code, 201)
         
+
         
