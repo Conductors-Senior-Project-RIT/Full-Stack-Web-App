5c0df905fc6118cd27bd61216664c690927cf05e
“Seatea22” <cjt7922@rit.edu>
2026-04-16 15:00:38 -0400

    A lot more documentation added to repository.py, implemented tests for: constructor, get, update, update_with_pk, aggregated these tests with based_record_repo in test_base_repo.py

--- backend/src/db/db_core/repository.py
+++ backend/src/db/db_core/repository.py
@@ -3,6 +3,7 @@ from typing import Any, Generic, Protocol, Type, TypeVar, Union, runtime_checkab
 
 from sqlalchemy import Row, Sequence, inspect
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoInspectionAvailable
 
 from .exceptions import (
     RepositoryInvalidArgumentError, RepositoryNotFoundError, RepositoryParsingError, 
@@ -71,9 +72,11 @@ class BaseRepository(Generic[ModelType]):
         # Extract the primary key by inspecting the model's attributes
         self.pkey = None
         if self.model:
-            pkeys = inspect(self.model).primary_key
-            if pkeys:
+            try:
+                pkeys = inspect(self.model).primary_key
                 self.pkey = pkeys[0].name if pkeys else None
+            except NoInspectionAvailable:
+                pass
         
         
     @repository_error_handler()
@@ -100,62 +103,135 @@ class BaseRepository(Generic[ModelType]):
         """
         obj = self.session.get(self.model, pkey)
         
-        # Throw error if row is not found
+        # If an object cannot be found with the provided primary key, raise an error
         if not obj:
             raise RepositoryNotFoundError(
                 self.__class__.__name__, "get",
                 f"Could not find row in {self.model} with a pkey = {pkey}", True  
             )
         
+        # Return the retrieved instannce, either as its original ModelType or dictionary representation.
         return self.objs_to_dicts(obj) if to_dict else obj
     
     
     @repository_error_handler()
-    def update(self, objs: list[tuple[ModelType, dict[str, Any]]], to_dict=True) -> CollectionResult:        
-        # Return None if empty or undefined
+    def update(self, objs: list[tuple[ModelType, dict[str, Any]]], to_dict=True) -> CollectionResult:  
+        """
+        Updates the provided objects with the provided new values. 
+        
+        The `objs` parameter is a list of tuples, where each tuple contains an `ModelType` instance 
+        to update and a dictionary of new values to update that instance with. By default, this method 
+        returns a list of dictionary representations of the updated results, which can be turned off by 
+        setting `to_dict` to `False` to instead return a list of `ModelType` instances. 
+
+        
+        Args:
+            objs (list[tuple[ModelType, dict[str, Any]]]): A list of tuples, where each tuple contains an 
+                `ModelType` instance (index 0) to update and a dictionary of new values to update that instance 
+                with (index 1). The keys in the dictionary should correspond to column names in the table, and the 
+                values should be the new values to update those columns with. To prevent updates to the primary key, 
+                any keys in the update dictionaries that match the primary key column ({self.pkey}) are ignored.
+                An error will be thrown if any of the update dictionaries contain keys or values that are incompatible
+                with the model's attributes or corresponding column types.
+            
+            to_dict (bool, optional): Specifies whether updated instances should be returned as a `ModelType` or `dict`. 
+                Setting this field to `True` returns the results as a `dict`; otherwise, a `ModelType`. Default value is True.
+                
+        Raises:
+            RepositoryInvalidArgumentError: Thrown if any of the provided objects are not instances of `ModelType` 
+                or if any of the provided update keys are not attributes of the table.
+            RepositoryParsingError: Thrown if any of the provided update values are incompatible with the corresponding 
+                column types in the model.
+                
+        Returns:
+            CollectionResult: A list of `ModelType` or `dict` instance containing the updated results, depending on the 
+                value of `to_dict`. If the provided `objs` is empty, an empty list is returned. Additionally, if no updates
+                are made to the provided objects, an empty list is returned.
+                
+        Notes:
+            - Other `RepositoryError` exceptions may be thrown depending on errors raised when performing database operations, 
+            such as connection errors or internal errors.
+        """
+              
+        # Return an empty list if the provided values to update is empty
         if not objs:
             return []
 
-        # Update all objects
+        # Maintain a list of the objects that were updated to return at the end.
+        # This prevents returning objects that were not actually updated, such as when new values are the same as current ones.
         updated = []
         for obj, updates in objs:
-            # The object must be a correct type
+            # Raise an error if the provided object is not an instance of the model
             if not issubclass(obj.__class__, Base):
                 raise RepositoryInvalidArgumentError(
                     self.__class__.__name__, "update",
                     "Object must be a valid model instance!", True
                 )
             
-            # Now update all objs
+            # Maintain a boolean to track whether an update was made to the current object.
             has_updated = False
             for key, new_value in updates.items():
+                # Ignore any updates to the primary key to prevent complications
                 if key == self.pkey:
                     continue
                 
+                # If the object does not have the specified attribute to update, raise an error
                 if not hasattr(obj, key):
                     raise RepositoryInvalidArgumentError(
                         self.__class__.__name__, "update",
                         f"Column name '{key}' not found in {obj}!", True
                     )
                 
+                # Retrieve the current value of the object provided, and check to see if an update is necessary.
+                # If the values differ, then set the new value for the object and update the boolean to reflect that an update was made.
                 current_value = getattr(obj, key)
                 if current_value != new_value:
                     has_updated = True
                     setattr(obj, key, new_value)
             
+            # If an update was made, then add it to the list of updated objects to return
             if has_updated:
                 updated.append(obj)
         
-        # Flush to reflect changes in session
+        # Flush updates to reflect changes in the current session
         self.session.flush()
         
+        # Return the updated objects, either as their original ModelType or dictionary representation.
         return self.objs_to_dicts(updated) if to_dict else updated
     
     
     @repository_error_handler()
     def update_with_pk(self, pkey: int | str, new_values: dict[str, Any], to_dict=True) -> SingleResult | None:
+        """
+        Updates a single ORM instance in the current session.
+        
+        If an instance can be found in the session from a provided primary key (`pkey`), its values will be updated to 
+        those present in `new_values`. Similar to other functions, the updated instance can be returned as a `ModelType`
+        or dictionary representation depending on the value of `to_dict`. If no updates are made, this function will
+        return `None`.
+
+        Args:
+            pkey (int | str): _description_
+            new_values (dict[str, Any]): _description_
+            to_dict (bool, optional): _description_. Defaults to True.
+            
+        Raises:
+            RepositoryInvalidArgumentError: Thrown if any of the provided update keys are not attributes of the table.
+            RepositoryParsingError: Thrown if any of the provided update values are incompatible with the corresponding 
+                column types in the model.
+            RepositoryNotFoundError: Thrown if an instance cannot be found in the current session with the provided `pkey`.
+
+        Returns:
+            SingleResult | None: A `ModelType` or `dict` instance containing the updated results, depending on the 
+                value of `to_dict`. If no updates are made, None is returned.
+        """
+        # Get the instance to update with a matching primary key, and raise an error if it cannot be found
         obj = self.get(pkey, to_dict=False)
+        
+        # Use the update function to perform an update on the retrieved instance with the provided new values
         result = self.update([(obj, new_values)], to_dict)
+        
+        # If there is exactly one updated result, return that result; otherwise, return None to indicate that no updates were made
         return result[0] if len(result) == 1 else None
         
         
@@ -179,8 +255,8 @@ class BaseRepository(Generic[ModelType]):
         self.session.flush()
         
             
-    @repository_error_handler()
     @classmethod
+    @repository_error_handler()
     def objs_to_dicts(cls, values: AsDictConvertible | Sequence[AsDictConvertible], convert_to_string: set[str] = {}) -> dict[str, Any] | list[dict[str, Any]]:
         # Determine if the values given is iterable
         is_collection = isinstance(values, (Iterable))
